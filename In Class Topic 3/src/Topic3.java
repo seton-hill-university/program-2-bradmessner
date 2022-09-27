9bceef3c0f36d931a938e66566e7b5847f88bdb1
Brad D. Messner <bmessner@setonhill.edu>
2022-09-27 09:59:49 -0400

    :)

---
+++ In Class Topic 3/src/Topic3.java
@@ -0,0 +1,54 @@
+// Header
+
+
+import java.util.Scanner;
+
+public class Topic3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Activate keyboard
+	// TODO Auto-generated method stub
+		
+		// Activate scanner/keyboard
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Main Menu - Overwatch");
+		System.out.println("\t1.) Start Game");
+		System.out.println("\t2.) Highlights");
+		System.out.println("\t3.) High Scores");
+		System.out.println("\t4.) Exit");
+		System.out.print("What item would you like? ");
+		
+		// Placeholder to ask for user input later
+		int menuChoice = keyboard.nextInt();
+		int highScore = 5000;
+		
+		switch (menuChoice) {
+			case 1:
+				System.out.println("Loading Game....");
+				break;
+			case 2:
+				System.out.println("Let's watch those highlights");
+				break;
+			case 3: 
+				System.out.println("Highest Score: Brad");
+				System.out.println("Lowest Score: Dr. Hoover");
+				System.out.println("Brad > Dr. Hoover");
+				
+				if (highScore > 4000)
+					System.out.println("So Close");
+				else
+					System.out.println("Get more practice");
+				
+				break;
+			case 12:
+				break;
+			default:
+				System.out.println("Thanks for playing");	
+		}
+
+	}
+
+}
